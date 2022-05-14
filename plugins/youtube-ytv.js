6bd5b2e7c6da9bffe6253f0abbe011379c431ef5
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-05-14 16:56:54 +0700

    Update youtube-ytv.js

--- plugins/youtube-ytv.js
+++ plugins/youtube-ytv.js
@@ -26,22 +26,22 @@ let handler = async (m, { conn, args, isPrems, isOwner }) => {
   }
   if ((!(source instanceof ArrayBuffer) || !link || !res.ok) && !isLimit) throw 'Error: ' + (lastError || 'Can\'t download video')
   if (!isY && !isLimit) await conn.sendFile(m.chat, thumbnail, 'thumbnail.jpg', `
-*📌Title:* ${title}
-*🗎 Filesize:* ${video.fileSizeH}
+*Title:* ${title}
+*Filesize:* ${video.fileSizeH}
 *${isLimit ? 'Pakai ' : ''}Link:* ${link}
 `.trim(), m)
   let _thumb = {}
   try { _thumb = { thumbnail: await (await fetch(thumbnail)).buffer() } }
   catch (e) { }
   if (!isLimit) await conn.sendFile(m.chat, link, title + '.mp4', `
-*📌Title:* ${title}
-*🗎 Filesize:* ${video.fileSizeH}
+*Title:* ${title}
+*Filesize:* ${video.fileSizeH}
 `.trim(), m, false, {
     ..._thumb,
     asDocument: chat.useDocument
   })
 }
-handler.help = ['mp4', 'v', ''].map(v => 'yt' + v + ` <url> <without message>`)
+handler.help = ['mp4', 'v', ''].map(v => 'yt' + v + ` <url>`)
 handler.tags = ['downloader']
 handler.command = /^yt(v|mp4)?$/i
 
@@ -49,4 +49,3 @@ handler.exp = 0
 
 
 export default handler
-
